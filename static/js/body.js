efe664f9595aa4e2f32a95840585c78c9077ba48
古映杰 <yj_gu@Ctrip.com>
2015-05-07 16:49:52 +0800

    save

--- static/js/body.js
+++ static/js/body.js
@@ -1,12 +1,38 @@
 var React = require('react')
-var Box = require('./box')
-var Text = require('./text')
+var mui = require('material-ui')
+var DatePicker = mui.DatePicker
+var FontIcon = mui.FontIcon
+var Slider = mui.Slider
+var FlatButton = mui.FlatButton
+var RaisedButton = mui.RaisedButton
+var FloatingActionButton = mui.FloatingActionButton
+var Dialog = mui.Dialog
+var Paper = mui.Paper
+var TextField = mui.TextField
+var DropDownMenu = mui.DropDownMenu
+var Snackbar = mui.Snackbar
+
+var menuItems = [
+   { payload: '1', text: 'Never' },
+   { payload: '2', text: 'Every Night' },
+   { payload: '3', text: 'Weeknights' },
+   { payload: '4', text: 'Weekends' },
+   { payload: '5', text: 'Weekly' },
+];
+
+
+
+
 
 var Body = React.createClass({displayName: "Body",
 	render: function() {
 		return (
-			React.createElement(Box, null, 
-				React.createElement(Text, null, this.props.data.data)
+			React.createElement("div", null, 
+			React.createElement(Slider, {name: "slider1"}), 
+			React.createElement(DropDownMenu, {menuItems: menuItems}), 
+			React.createElement(DatePicker, {
+  hintText: "Landscape Dialog", 
+  mode: "landscape"})
 			)
 			)
 	}
--- static/jsx/body.js
+++ static/jsx/body.js
@@ -1,13 +1,39 @@
 var React = require('react')
-var Box = require('./box')
-var Text = require('./text')
+var mui = require('material-ui')
+var DatePicker = mui.DatePicker
+var FontIcon = mui.FontIcon
+var Slider = mui.Slider
+var FlatButton = mui.FlatButton
+var RaisedButton = mui.RaisedButton
+var FloatingActionButton = mui.FloatingActionButton
+var Dialog = mui.Dialog
+var Paper = mui.Paper
+var TextField = mui.TextField
+var DropDownMenu = mui.DropDownMenu
+var Snackbar = mui.Snackbar
+
+var menuItems = [
+   { payload: '1', text: 'Never' },
+   { payload: '2', text: 'Every Night' },
+   { payload: '3', text: 'Weeknights' },
+   { payload: '4', text: 'Weekends' },
+   { payload: '5', text: 'Weekly' },
+];
+
+
+
+
 
 var Body = React.createClass({
 	render: function() {
 		return (
-			<Box>
-				<Text>{this.props.data.data}</Text>
-			</Box>
+			<div>
+			<Slider name="slider1" />
+			<DropDownMenu menuItems={menuItems} />
+			<DatePicker
+  hintText="Landscape Dialog"
+  mode="landscape"/>
+			</div>
 			)
 	}
 })
